2af5b860ec43af4d62c080e67611bac657a05417
prashantbhadoria1902 <prashantbhadoria1902@gmail.com>
2025-03-07 01:31:28 +0530

    [Prashant] Added UC4 Assignment

--- src/main/java/com/example/controller/AddressBookController.java
+++ src/main/java/com/example/controller/AddressBookController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/addressbook")
 public class AddressBookController {
-
+    
     @Autowired
     private AddressBookService service;
 
--- src/main/java/com/example/model/AddressBook.java
+++ src/main/java/com/example/model/AddressBook.java
@@ -12,7 +12,6 @@ public class AddressBook {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String email;
     private String phone;
